1e63dc02a2ca53573c52a86b6956642e80e4f8f4
Nguyen Phuc Thien An <npta97@gmail.com>
2018-12-07 10:07:46 +0700

    Update Manager entity

--- Core/Models/Manager.cs
+++ Core/Models/Manager.cs
@@ -15,6 +15,7 @@ namespace PremierLeagueAPI.Core.Models
         public string Name { get; set; }
         public string Nationality { get; set; }
         public DateTime Birthdate { get; set; }
+        public string Description { get; set; }
         public string PhotoUrl { get; set; }
         public ICollection<SquadManager> SquadManagers { get; set; }
     }
