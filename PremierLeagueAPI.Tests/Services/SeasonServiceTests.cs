dfeb9674bec22cb0ef4ce290d3bbd205fbca0a7b
Nguyen Phuc Thien An <npta97@gmail.com>
2018-12-20 11:47:01 +0700

    Add unit tests for SeasonService

---
+++ PremierLeagueAPI.Tests/Services/SeasonServiceTests.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PremierLeagueAPI.Core;
+using PremierLeagueAPI.Core.Models;
+using PremierLeagueAPI.Core.Queries;
+using PremierLeagueAPI.Core.Repositories;
+using PremierLeagueAPI.Helpers;
+using PremierLeagueAPI.Services;
+
+namespace PremierLeagueAPI.Tests.Services
+{
+    [TestFixture]
+    public class SeasonServiceTests
+    {
+        private Mock<IUnitOfWork> _unitOfWork;
+        private Mock<ISeasonRepository> _seasonRepository;
+        private SeasonService _seasonService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _seasonRepository = new Mock<ISeasonRepository>();
+
+            _seasonService = new SeasonService(_unitOfWork.Object, _seasonRepository.Object);
+        }
+
+        [Test]
+        public async Task GetAsync_WhenCalled_GetSeasonsFromDb()
+        {
+            var seasonQuery = new SeasonQuery
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                SortBy = "name",
+                IsSortAscending = true
+            };
+
+            var expectedSeasons = new PaginatedList<Season>
+            {
+                Pagination = new Pagination
+                {
+                    PageNumber = 1,
+                    PageSize = 10
+                },
+                Items = new List<Season>()
+                {
+                    new Season {Id = 1},
+                    new Season {Id = 2},
+                    new Season {Id = 3},
+                }
+            };
+
+            _seasonRepository.Setup(s => s.GetAsync(seasonQuery)).ReturnsAsync(expectedSeasons);
+
+            var result = await _seasonService.GetAsync(seasonQuery);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedSeasons, result);
+        }
+
+        [Test]
+        public async Task GetBriefListAsync_WhenCalled_GetSeasonsFromDb()
+        {
+            var expectedSeasons = new List<Season>()
+            {
+                new Season {Id = 1},
+                new Season {Id = 2},
+                new Season {Id = 3},
+            };
+
+            _seasonRepository.Setup(s => s.GetBriefListAsync()).ReturnsAsync(expectedSeasons);
+
+            var result = await _seasonService.GetBriefListAsync();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedSeasons, result);
+        }
+
+        [Test]
+        public async Task GetAsync_WhenCalled_GetSeasonFromDb()
+        {
+            const int id = 1;
+            var expectedSeason = new Season
+            {
+                Id = id
+            };
+
+            _seasonRepository.Setup(s => s.GetAsync(id)).ReturnsAsync(expectedSeason);
+
+            var result = await _seasonService.GetByIdAsync(id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedSeason, result);
+        }
+
+        [Test]
+        public async Task GetAsync_WhenCalled_GetSeasonDetailFromDb()
+        {
+            const int id = 1;
+            var expectedSeason = new Season
+            {
+                Id = id
+            };
+
+            _seasonRepository.Setup(s => s.GetDetailAsync(id)).ReturnsAsync(expectedSeason);
+
+            var result = await _seasonService.GetDetailByIdAsync(id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedSeason, result);
+        }
+
+        [Test]
+        public async Task CreateAsync_WhenCalled_CreateNewSeason()
+        {
+            var season = new Season
+            {
+                Id = 1
+            };
+
+            await _seasonService.CreateAsync(season);
+
+            _seasonRepository.Verify(s => s.Add(season), Times.Once);
+            _unitOfWork.Verify(u => u.CompleteAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task UpdateAsync_WhenCalled_UpdateExistingSeason()
+        {
+            var season = new Season
+            {
+                Id = 1
+            };
+
+            await _seasonService.UpdateAsync(season);
+
+            _unitOfWork.Verify(u => u.CompleteAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task DeleteAsync_WhenCalled_DeleteSeasonFromDb()
+        {
+            var season = new Season
+            {
+                Id = 1
+            };
+
+            await _seasonService.DeleteAsync(season);
+
+            _seasonRepository.Verify(s => s.Remove(season), Times.Once);
+            _unitOfWork.Verify(u => u.CompleteAsync(), Times.Once);
+        }
+    }
+}
